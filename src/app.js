ae99eb1dc52ed84282e28395e890447713d8eec0
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-15 01:40:00 +0000

    added background image and added boot strap color generation to js file

--- src/app.js
+++ src/app.js
@@ -5,21 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
   let end = '.com'
 
-  for(let index in pronoun){
-    for(let secondIndex in adj){
-      for(let thirdIndex in noun){
-        let myUL = document.getElementById("myUl");
-        let myLi = document.createElement("li");
-        
-        myLi.textContent = pronoun[index] + adj[secondIndex] + noun[thirdIndex] + end;
-        myUL.appendChild(myLi);
+  const bootstrapColors = [
+    "text-primary",
+    "text-secondary",
+    "text-success",
+    "text-danger",
+    "text-warning",
+    "text-info",
+    "text-light",
+    "text-dark"
+  ];
+
+  let rand = Math.floor(Math.random(bootstrapColors) * bootstrapColors.length);
+
+  for (let index in pronoun) {
+    for (let secondIndex in adj) {
+      for (let thirdIndex in noun) {
+         let myUL = document.getElementById("myUl");
+          let myLi = document.createElement("li");
+          const element = document.getElementById("myUl");
+
+          myLi.textContent = pronoun[index] + adj[secondIndex] + noun[thirdIndex] + end;
+          myUL.appendChild(myLi);
+          element.className = bootstrapColors[rand];
       }
     }
   }
